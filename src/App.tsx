2643569beb11f7813af3461990cd6c380c8fa073
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-11-09 20:47:28 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Main } from "./styled-components/AppStyled"
+
+function App() {
+  return (
+    <Main>
+      
+    </Main>
+  )
+}
+
+export default App
---
+++ src/styled-components/AppStyled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Main = styled.div`
+    width: 100%;
+    min-height: 100vh;
+`
+
+export {Main}
